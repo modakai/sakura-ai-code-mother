ce3ad214a0a7ec4541f3394221fb0754ef5f7957
Sakura <2049202552@qq.com>
2025-10-28 20:23:56 +0800

    feat(api): 添加 selenium 自动化网页截图工具

---
+++ sakura-ai-code-mother-api/src/main/java/com/sakura/aicode/job/ExitIocListener.java
@@ -0,0 +1,13 @@
+package com.sakura.aicode.job;
+
+import com.sakura.aicode.utils.WebScreenshotUtil;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.stereotype.Component;
+@Component
+public class ExitIocListener implements ApplicationListener<ContextClosedEvent> {
+   @Override
+   public void onApplicationEvent(ContextClosedEvent event) {
+       WebScreenshotUtil.destroy();
+   }
+}
